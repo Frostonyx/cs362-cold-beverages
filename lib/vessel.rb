a9b660454e4c4f7acd05ad6abe0fa19f7d73abde
Frostonyx <danielhelm18@gmail.com>
2023-03-20 23:35:19 -0700

    fixed implementation of vessel.rb

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,8 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume
 
-  def initialize(name, volume)
+  def initialize(name='FAKE, volume=100)
     @name = name
     @volume = volume
   end
-
 end
