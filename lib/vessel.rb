8ca13348c38c3408e5f9981a3cad36ec82433785
Frostonyx <danielhelm18@gmail.com>
2023-03-21 00:03:45 -0700

    all vessel_spec tests now pass

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,8 +1,17 @@
 class Vessel
   attr_reader :name, :volume
 
-  def initialize(name='FAKE, volume=100)
+  def initialize(name='FAKE', volume=100)
     @name = name
     @volume = volume
+    @current_volume = 0
+  end
+
+  def empty?
+    @current_volume == 0
+  end
+
+  def fill
+    @current_volume = @volume
   end
 end
